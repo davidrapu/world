89996c30bf734480181a9641b77bf1f5182f7edd
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-10-15 17:44:14 +0100

    testing usage of params and queries as states

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,16 @@
+import { useSearchParams } from "react-router";
 import styles from "./Map.module.css";
 
 function Map() {
+  const [searchParam, setSearchParam] = useSearchParams()
+  const [lat, lng] = [searchParam.get("lat"), searchParam.get("lng")]
   return <section className={styles.map}>
-    Map
+    <p>
+      Latitude: {lat}
+    </p>
+    <p>
+    Langtitude: {lng}
+    </p>
   </section>;
 }
 
