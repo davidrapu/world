3a97eacf6ac03a862298abe50de941cade5b889d
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-10-15 17:43:52 +0100

    countries page design ok for mobile

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,24 @@
-import styles from './Countries.module.css'
+import styles from "./Countries.module.css";
 
-function Countries() {
-    return (
-        <div className={styles.countries}>
-            Countries
-        </div>
-    )
+function Countries({ cities }) {
+    const countries = cities.reduce((arr, cityObj) => {
+        if (!arr.map(obj => obj.country).includes(cityObj.country))
+            return [...arr, {id: cityObj.id, country: cityObj.country, emoji: cityObj.emoji}]
+        else return arr
+    }, [])
+  return (
+    <div className={styles.countries}>
+      {countries.map((countryObj) => (
+        <Country key={countryObj.id} countryIcon={countryObj.emoji} countryName={countryObj.country} />
+        ))}
+    </div>
+  );
+}
+function Country({ countryIcon, countryName }) {
+  return <div className={styles.country}>
+    <span> {countryIcon} </span>
+    <p> {countryName} </p>
+  </div>;
 }
 
-export default Countries
+export default Countries;
